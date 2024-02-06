1a7706e6f25ee207b29813740be332d62edfab00
sandiarbaa <sandiarbaa@gmail.com>
2024-02-06 17:50:37 +0700

    4.Implementasi Atomic Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-// Untuk menggunakan dependencies React di pembuatan component menggunakan Class Component
 import React from "react";
-import Button from "./components/Elements/Button";
-
-// Pembuatan Component menggunakan Class Component
-// class Button extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
-//         type="submit"
-//       >
-//         Buy now
-//       </button>
-//     );
-//   }
-// }
-
-// Pembuatan Component menggunakan Function Component
-// function ButtonBlack() {
-//   return (
-//     <button
-//       className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-//       type="submit"
-//     >
-//       Buy now
-//     </button>
-//   );
-// }
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-3">
-        <Button variant="bg-red-700">Login</Button>
-        <Button variant="bg-slate-700">Logout</Button>
-        <Button></Button>
-      </div>
+    <div className="flex justify-center min-h-screen items-center">
+      {/* <LoginPage /> */}
+      <RegisterPage />
     </div>
   );
 }
--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,10 +1,10 @@
 // Pembuatan Component menggunakan Arrow Function
 const Button = (props) => {
   // ada parameter props (object) di setiap component di react, dan bisa diberi nilai default tiap key object nya, dengan destructuring
-  const { children, variant = "bg-black" } = props;
+  const { children, classname = "bg-black" } = props;
   return (
     <button
-      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`}
       type="submit"
     >
       {children}
