337449b778d1a8cf52e7162110af4c60fe158f0d
sandiarbaa <sandiarbaa@gmail.com>
2024-02-10 11:49:38 +0700

    12.Lifecycle (class component)

--- src/components/Fragments/Counter.jsx
+++ src/components/Fragments/Counter.jsx
@@ -5,9 +5,20 @@ class Counter extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      //   nama: "sandi",
     };
-    // console.log(this.state);
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    this.setState({ count: 1 });
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    if (prevState.count === 10) {
+      this.setState({ count: 0 });
+    }
   }
 
   render() {
@@ -17,18 +28,32 @@ class Counter extends React.Component {
         <button
           className="bg-black text-white p-3"
           onClick={() => this.setState({ count: this.state.count + 1 })}
-          //   onClick={() =>
-          //     this.setState(
-          //       { count: this.state.count + 1, nama: "arba" },
-          //       console.log(this.state.nama)
-          //     )
-          //   }
         >
           +
         </button>
+        {console.log("render")}
       </div>
     );
   }
 }
 
 export default Counter;
+
+// Konsep Dasar LifeCycle pada sebuah Class Component
+
+// 1. Mounting
+// Pertama akan ke CONSTRUCTOR(penentuan nilai awal state, kalau memang ada state yg di buat), lalu ke RENDER elemen jsx nya, lalu kalau ada perubahan state maka akan ke COMPONENTDIDMOUNT baru perubahan akan di render lagi, karena dia akan mendeteksi perubahan pada state.
+
+// jadi apa yg ada di constructor akan di timpa oleh componentDidMount
+// ini biasanya berhubungan dengan pengambilan data lewat API,
+// jadi setelah di definisikan sebuah state lalu kita misalkan mengambil data lewat API, nah mengambil data lewat API itu tidak dilakukan di constructor, melainkan di componentDidMount
+
+// render akan terjadi ketika component di mounting atau ada update baru.
+
+// 2. Updating
+// componentDidUpdate dia akan mencoba melihat perubahan yg terjadi pada component tersebut.
+
+// berbeda dengan componentDidMount, kalau componentDidMount dia hanya akan dijalankan sekali ketika component pertama kali di render, lalu ketika ada perubahan maka baru componentDidUpdate akan dijalankan, seperti misal ada perubahan state atau ada penambahan props baru, dll.
+
+// 3. Unmounting
+// ketika component dihancurkan/tidak di pakai lagi atau dihilangkan dari website nya, maka itu termasuk ke dalam unmount.
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
+import Counter from "../components/Fragments/Counter";
 
 const products = [
   {
@@ -141,6 +142,9 @@ const ProductPage = () => {
             </tbody>
           </table>
         </div>
+      </div>
+      <div className="my-5 flex justify-center">
+        <Counter />
       </div>
     </Fragment>
   );
