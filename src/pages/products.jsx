66d691467bce035a32136849f4ea306212823d58
sandiarbaa <sandiarbaa@gmail.com>
2024-02-25 19:58:11 +0700

    17.Custom Hooks

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -4,25 +4,16 @@ import Button from "../components/Elements/Button";
 import Counter from "../components/Fragments/Counter";
 import { getProducts } from "../services/product.service";
 import { getUsername } from "../services/auth.service";
+import { useLogin } from "../hooks/useLogin";
 
 const ProductPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const [username, setUsername] = useState("");
+  const username = useLogin();
 
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
-  }, []);
-
-  // validasi token
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setUsername(getUsername(token));
-    } else {
-      window.location.href = "/login";
-    }
   }, []);
 
   useEffect(() => {
